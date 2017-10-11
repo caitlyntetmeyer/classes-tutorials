f9fb5308f98695181035792048662705cd8cbd8e
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-10-11 17:03:15 -0400

    Bob's Big Giveaway game

---
+++ csharp-dotnet-dnn-azure/microsoft-virtual-academy-csharp/Decisions/Decisions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Decisions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            // decision statement:
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "2")
+            {
+                string message = "You won a new boat!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                string message = "You won a new cat!";
+                Console.WriteLine(message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
