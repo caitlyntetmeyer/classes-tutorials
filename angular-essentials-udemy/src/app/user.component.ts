32bc097b28693dbddb3d4549b67014a82dcc54a5
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-10-30 20:27:58 -0400

    data binding

--- angular-essentials-udemy/src/app/user.component.ts
+++ angular-essentials-udemy/src/app/user.component.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
+  	<input type="text"
+  		/* 
+
+  		Event binding (listening for an event): (input)="onUserInput($event)" 
+  		Property binding (passing data to element properties): [value]="name" 
+  		Or, combine them with two-way data binding:
+
+  		*/
+  		[(ngModel="name")]>
+  	// String interpolation (outputting text in a template):
     <p>Hello, {{ name }}!</p>
     <p>I'm the user component</p>
     `
 })
 export class UserComponent {
 	name = 'Caitlyn';
+
+	onUserInput(event) {
+		this.name = event.target.value;
+	}
 }
