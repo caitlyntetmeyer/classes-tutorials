e993040d99d4682d89ecd760472606c20af45224
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-11-01 11:39:13 -0400

    completed Understanding C# Class Files

--- csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/Classes/Classes/Program.cs
+++ csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/Classes/Classes/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classes
+namespace ClassDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Animal newAnimal = new Animal();
+            newAnimal.move();
 
         }
     }
