ba6c70e5084a54c34f29fa6f79602e126c469d53
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-11-03 20:26:56 -0400

    completed Exports and Imports

--- udemy-react/udemy-react.js
+++ udemy-react/udemy-react.js
@@ -49,3 +49,38 @@ console.log(multiply(2));       // 4
 // Additional shortening possibility, if exactly 1 argument AND only returning sth AND :
 const multiply = number => number * 2;  // no () around "number" AND no "return" keyword AND everything on one line
 console.log(multiply(2));   // 4
+
+/*
+
+---
+
+Exports & Imports (Modules):
+
+https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8211788?start=0
+
+Ex. 1 - In 1st file, person.js:
+*/
+const person = {
+    name: 'Max'
+}
+// "default" keyword means, "If you export something from this file, it should be this thing":
+export default person
+
+// Ex. 1 - In 2nd file, utility.js:
+export const baseData = 10;
+export const clean = () => {...}
+
+// Ex. 1 - in 3rd file, app.js:
+import person from './person.js' // or "import prs from './person.js'" - it's up to you what you name the default
+
+// Still in 3rd file - _named_ exports (you must use the exact name, between curly braces):
+import {baseData} from './utility.js'
+import {clean} from './utility.js'  
+// Alternatively, you could create an _alias_ using the "as" keyword:
+import {clean as Clean} from './utility.js'
+// Still alternatively, you could use an asterisk with "as bundled":
+import * as bundled from './utility.js'
+
+
+
+
