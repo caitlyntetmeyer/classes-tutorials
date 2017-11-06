5662b9fd63257de3976cadf232be1adf04ef970e
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-11-06 14:10:31 -0500

    started strings

---
+++ csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/strings/strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace strings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Escape sequences:
+            \'      // single quote
+            \b      // backspace
+            \n      // new line
+            \t      // tab
+        }
+    }
+}
