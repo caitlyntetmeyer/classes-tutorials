3c546e66dd50d0b288dc5a96f0fcc43e9ed51102
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-11-06 12:29:30 -0500

    began YouTube C# tutorial, including do...while loops

---
+++ csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/youtube-csharp-tutorial/youtube-csharp-tutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace youtubecsharptutorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string guess;
+            // Have the user guess a number until they guess the number 15:
+            do
+            {
+                Console.WriteLine("Guess a number.");
+                guess = Console.ReadLine();
+
+            } while (!guess.Equals("15"));  // Keep asking the user to guess until they guess 15.
+        }
+    }
+}
