04c6cc5da66fdcb2e36ac04d3df9ce1a0b55e490
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-11-06 15:51:02 -0500

    continued learning C#

---
+++ csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/Destructors/Destructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Destructors
+{
+    public static void Main(string[] args)
+    {
+        class Dog
+    {
+        public Dog()    // constructor
+        {
+            // initialization statements here
+        }
+
+        ~Dog()      // destructor
+        {
+            // cleanup statements here
+        }
+    }
+}
+}
--- csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/strings/strings/Program.cs
+++ csharp-dotnet-dnn-azure/lynda-csharp-exercise-files/strings/strings/Program.cs
@@ -6,11 +6,25 @@ namespace strings
     {
         public static void Main(string[] args)
         {
-            // Escape sequences:
+            /* Escape sequences:
             \'      // single quote
+            \"      // double quote
             \b      // backspace
             \n      // new line
             \t      // tab
+            */
+            string sampString = "A bunch of random words";
+
+            Console.WriteLine("Is empty: " + String.IsNullOrEmpty(sampString));
+
+            Console.WriteLine("Is empty: " + String.IsNullOrWhiteSpace(sampString));
+
+            Console.WriteLine("String Length: " + sampString.Length);
+            /*
+            Is empty: False
+            Is empty: False
+            String Length: 23
+            */
         }
     }
 }
