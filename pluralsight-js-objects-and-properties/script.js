76a9a36e2dd1cf6336182a49a40e040cba3c87b1
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-12-01 14:13:16 -0500

    added more on prototypes

--- pluralsight-js-objects-and-properties/script.js
+++ pluralsight-js-objects-and-properties/script.js
@@ -22,20 +22,85 @@
 //   display(cat.name.first)
 //   display(cat.name.last)
 
-// var arr = ['red', 'blue', 'green']
+// ---
 
+// var arr = ['red', 'blue', 'green']
 // Object.defineProperty(arr, 'last', {get: function() {
 //   return this[this.length-1]
 // }})
 // var last = arr.last
 // display(last)
 
-var arr = ['red', 'blue', 'green']
+// ---
 
-Object.defineProperty(Array.prototype, 'last', {get: function() {
-  return this[this.length-1]
-}})
-var last = arr.last
-var arr2 = ['one', 'two', 'three']
-display(arr.last);
-display(arr2.last);
+// var arr = ['red', 'blue', 'green']
+// // This works the same: var arr = new Array('red', 'blue', 'green')
+
+// Object.defineProperty(Array.prototype, 'last', {get: function() {
+//   return this[this.length-1]
+// }})
+// var last = arr.last
+// var arr2 = ['one', 'two', 'three']
+// display(arr.last);  // green
+// display(arr2.last); // three
+
+// ---
+
+// var myFunc = function() {}
+// display(myFunc.prototype)
+
+// var cat = {name: 'Fluffy'}
+// display(cat.prototype)  // {}
+
+// ---
+
+function Cat(name, color) {
+  this.name = name
+  this.color = color
+}
+var fluffy = new Cat("Fluffy", "White")
+
+Cat.prototype.age = 3
+
+display(Cat.prototype)
+/* Cat {
+  age: 3
+} */
+display(fluffy.__proto__)
+/* Cat {
+  age: 3
+} */
+
+var muffin = new Cat("Muffin", "Brown")
+
+display(muffin.__proto__)
+/* Cat {
+  age: 3
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
