5bb2831ab226f3fde40f09e1a841b282f77ecb90
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-12-06 10:25:24 -0500

    first commit

---
+++ visual-studio-team-services.js
@@ -0,0 +1,13 @@
+/*
+
+Visual Studio Team Services
+Lynda.com course
+
+Team Management
+1) Alignment - ability to track work consistently across teams
+
+
+2) Autonomy - ability to choose how to manage work that's assigned to the team
+
+
+*/
