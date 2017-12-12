12796de4046599a3094cbf9e872737c705cd597f
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-12-12 14:06:07 -0500

    Angular Essentials: added ngModel in user component. Lynda Databases: completed notes on 'What Is a DB'

---
+++ lynda-databases.js
@@ -0,0 +1,19 @@
+/*
+
+Programming Foundations: Databases - Simon Allardice
+Lynda.com
+
+---
+
+What Are Databases?
+
+They solve the problems of data storage (names, descriptions, amounts, files):
+1. Size (ex: speed when you have 2 million entries)
+2. Ease of updating (ex: 200 ppl need to update it at the same time)
+3. Accuracy (ex: Nothing prevents typos in an Excel spreadsheet)
+4. Security (ex: Anyone could edit an Excel sheet)
+5. Redundancy (ex: The data itself shouldn't have multiple entries, esp. if they conflict)
+6. Importance of your data (ex: )
+
+
+*/
