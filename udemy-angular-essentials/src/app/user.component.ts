e160898af3eedc086e5e9cba3b3f75b76974b010
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-01-01 16:38:20 -0500

    Added text explaining what files certain paragraphs are coming from

--- udemy-angular-essentials/src/app/user.component.ts
+++ udemy-angular-essentials/src/app/user.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
+    <p>I'm from the user component (user.component.ts)!</p>
     <input type="text" [(ngModel)]="name">
     <p>Hello {{ name }}!</p>
-    <p>I'm the user component</p>
     `
 })
 export class UserComponent {
