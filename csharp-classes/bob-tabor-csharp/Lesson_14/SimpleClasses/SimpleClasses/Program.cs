7ebb3abbfa83add495e09e5221a8a71b2378c372
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-01-22 17:02:42 -0500

    Started classes

--- csharp-classes/bob-tabor-csharp/Lesson_14/SimpleClasses/SimpleClasses/Program.cs
+++ csharp-classes/bob-tabor-csharp/Lesson_14/SimpleClasses/SimpleClasses/Program.cs
@@ -11,36 +11,12 @@ namespace SimpleClasses
         static void Main(string[] args)
         {
             Car myCar = new Car();
-            myCar.Make = "Oldsmobile";
-            myCar.Model = "Cutlas Supreme";
-            myCar.Year = 1986;
-            myCar.Color = "Silver";
-
-            Console.WriteLine("{0} {1} {2} {3}", 
-                myCar.Make, 
-                myCar.Model, 
-                myCar.Year, 
-                myCar.Color);
-
-            //decimal value = DetermineMarketValue(myCar);
-            //Console.WriteLine("{0:C}", value);
-
-            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
-
-            Console.ReadLine();
         }
-
-        private static decimal DetermineMarketValue(Car car)
-        {
-            decimal carValue = 100.0M;
-
-            // Someday I might look up the car
-            // online webservice to get more 
-            // accurate value.
-            return carValue;
-        }
-
     }
+
+
+
+
 
     class Car
     {
@@ -49,18 +25,9 @@ namespace SimpleClasses
         public int Year { get; set; }
         public string Color { get; set; }
 
-        public decimal DetermineMarketValue()
-        {
-            decimal carValue;
-
-            if (Year > 1990)
-                carValue = 10000;
-            else
-                carValue = 2000;
-
-            return carValue;    
-        }
     }
 
+
+    // left off at 10:42 in video
 
 }
