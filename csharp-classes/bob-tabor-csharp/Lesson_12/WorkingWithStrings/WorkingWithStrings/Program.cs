e6f02e07bc24d13a3a492e956dda12cf66f66671
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-01-22 13:10:11 -0500

    Working with Strings, part 1

--- csharp-classes/bob-tabor-csharp/Lesson_12/WorkingWithStrings/WorkingWithStrings/Program.cs
+++ csharp-classes/bob-tabor-csharp/Lesson_12/WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -10,7 +10,32 @@ namespace WorkingWithStrings
     {
         static void Main(string[] args)
         {
-            string myString = "";
+            // Use a backslash to escape characters:
+            //string myString = "My \"so-called\" life";
+            //string myString = "What if I need a\nnew line?";
+            //string myString = "Go to your c:\\ drive";
+
+            // You can also use the @ symbol before the opening quotation to indicate that you want the whole thing to be a string:
+            //string myString = @"Go to your c:\ drive";
+
+            // Replacement Codes:
+            //string myString = String.Format("{0} = {1}", "First", "Second"); // First = Second
+            //string myString = String.Format("{1} = {0}", "First", "Second"); // Second = First
+
+            // Currency: Put ":C" immediately after the replacement code:
+            //string myString = string.Format("{0:C}", 123.45); // $123.45
+
+            // Alternative suggested by this code editor:
+            //string myString = $"{123.45}:C"; // $123.45
+
+            // To add in commas and decimal points, use ":N":
+            //string myString = string.Format("{0:N}", 1234567890); // 1,234,567,890.00
+
+            // To show sth as a percentage, use ":P":
+            // string myString = string.Format("Percentage: {0:P}", .123); // Percentage: 12.30%
+
+            // To do a phone number, use ":(###) ###-####":
+            string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890); // Phone Number: (123) 456-7890
 
             Console.WriteLine(myString);
             Console.ReadLine();
