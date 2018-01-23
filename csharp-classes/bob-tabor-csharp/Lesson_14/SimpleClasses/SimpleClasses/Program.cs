79c1ac6bd416b38530c0b2ed77e65315de50363c
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-01-23 10:35:56 -0500

    Completed car class exercise

--- csharp-classes/bob-tabor-csharp/Lesson_14/SimpleClasses/SimpleClasses/Program.cs
+++ csharp-classes/bob-tabor-csharp/Lesson_14/SimpleClasses/SimpleClasses/Program.cs
@@ -16,23 +16,56 @@ namespace SimpleClasses
             myCar.Year = 1986;
             myCar.Color = "Silver";
 
-            Console.WriteLine("{0} {1} {2} {3}", 
+            Console.WriteLine("{0} - {1} - {2} - {3}", 
                 myCar.Make, 
                 myCar.Model, 
                 myCar.Year, 
-                myCar.Color);
+                myCar.Color); // Oldsmobile - Cutlas Supreme - 1986 - Silver
             // We used myCar instead of Car because Car is just the blueprint. We want myCar, the variable name.
 
-            Console.ReadLine();
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value); // $100.00
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+            Console.ReadLine(); 
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+
+            // Someday, I might look up the car online using a web service to get a more accurate value.
+            return carValue;
         }
     }
 
+    // Create a new data type Car:
     class Car
     {
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+        // Longer version: prop public int MyProperty { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+
+            return carValue;
+            /*
+             * Oldsmobile - Cutlas Supreme - 1986 - Silver
+             * $2,000.00
+            */
+        }
+
 
     }
 
