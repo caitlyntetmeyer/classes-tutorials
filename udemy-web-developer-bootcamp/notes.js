c99317fdc3c4e10758da1f312ba045b54539875e
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-03-05 17:54:17 -0500

    HTML exercise

---
+++ udemy-web-developer-bootcamp/notes.js
@@ -0,0 +1,9 @@
+/*
+
+Introduction to HTML
+
+MDN: https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML
+
+
+
+*/
--- udemy-web-development-bootcamp/javascript-basics-control-flow/97-conditionals.js
+++
@@ -1,5 +0,0 @@
-if ( age < 18 ) {
-	console.log("Sorry, you're not old enough to enter the venue.");
-} else if ( age < 21 ) {
-	console.log("You can enter, but cannot drink.");
-}
