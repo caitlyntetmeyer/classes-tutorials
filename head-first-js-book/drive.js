4c93825776d1d0f424ee640a12862efd6966b1e4
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-04 17:43:55 -0400

    Fiat exercise

--- head-first-js-book/drive.js
+++ head-first-js-book/drive.js
@@ -70,17 +70,60 @@ var taxi = {
 	}
 };
 
-cadi.start();
-cadi.drive();
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "Medium Blue",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000,
+	started: false,
+	fuel: 0,
+	start: function() {
+		this.started = true;
+	},
+	stop: function() {
+		this.started = false;
+	},
+	drive: function() {
+		if (this.started) {
+			if (this.fuel > 0) {
+			alert(this.make + " " + this.model + " goes zoom zoom!");
+			this.fuel =- 1;
+			} else {
+				alert("Uh oh, out of fuel.");
+				this.stop();
+			}
+		} else {
+			alert("You need to start the engine first.");
+		}
+	},
+	addFuel: function(amount) {
+		this.fuel += amount;
+	}
+};
+
+cadi.start(); 
+cadi.drive(); // GM Cadillac goes zoom zoom!
 cadi.stop();
 
 chevy.start();
-chevy.drive();
+chevy.drive(); // Chevy Bel Air goes zoom zoom!
 chevy.stop();
 
 taxi.start();
-taxi.drive();
+taxi.drive(); // Webville Motors Taxi goes zoom zoom!
 taxi.stop();
+
+fiat.start();
+fiat.drive(); // Uh oh, out of fuel.
+fiat.addFuel(2); 
+fiat.start();
+fiat.drive(); // Fiat 500 goes zoom zoom!
+fiat.drive(); // Uh oh, out of fuel.
+fiat.drive(); // You need to start the engine first.
+fiat.stop();
 
 
 
