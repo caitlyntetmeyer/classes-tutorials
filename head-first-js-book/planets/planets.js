a424c035b773b2976f3020b74a34ea468eed9cff
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-13 19:10:13 -0400

    Added an attribute (class) using JS that turns the text of an element red

--- head-first-js-book/planets/planets.js
+++ head-first-js-book/planets/planets.js
@@ -5,6 +5,9 @@ var planet = document.getElementById("greenplanet");
 // Use the innerHTML property of the element object to change the content of the element:
 planet.innerHTML = "Red Alert: hit by phaser fire!";
 // So that paragraph on the page now reads, "Red Alert: hit by phaser fire!"
+
+// Add a class attribute that will turn the text of the element red:
+planet.setAttribute("class", "redtext");
 }
 
 window.onload = init;
