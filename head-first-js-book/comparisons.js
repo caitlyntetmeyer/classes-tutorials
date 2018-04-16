7028231f3ce0b4325a3aaeb36777624017178bd8
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-16 18:46:58 -0400

    Comparisons with == and ===

---
+++ head-first-js-book/comparisons.js
@@ -0,0 +1,10 @@
+"42" == 42; // true
+"42" === 42; // false
+"0" == 0; // true
+"0" === 0; // false
+"0" == false; // true
+"0" === false; // false
+"true" == true; // false
+"true" === true; // false
+true == (1 == "1"); // true
+true === (1 === "1"); // false
