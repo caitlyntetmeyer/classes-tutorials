291c646b4ef9b4a111af0d580a7a1d80dc377ca5
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-22 18:32:54 -0400

    instanceof exercise

---
+++ head-first-js-book/instanceof/instanceof.js
@@ -0,0 +1,13 @@
+function Duck(sound) {
+	this.sound = sound;
+	this.quack = function() {
+		console.log(this.sound); 
+	}
+}
+
+var toy = new Duck("quack quack");
+
+toy.quack(); // quack quack
+
+console.log(typeof toy); // object
+console.log(toy instanceof Duck); // true
