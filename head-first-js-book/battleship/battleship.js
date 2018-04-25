85ab4854fbdaf1ee583ba479d4942f1b6550acc2
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-25 19:32:23 -0400

    Planning the view object

---
+++ head-first-js-book/battleship/battleship.js
@@ -0,0 +1,15 @@
+var view = {
+	// This method takes a string message and displays it in the message display area.
+	displayMessage: function(msg) {
+		// code to be supplied in a bit!
+	},
+	displayMiss: function(location) {
+		// If the user guesses a location where there's no battleship, display MISS image
+	},
+	displayHit: function(location) {
+		// If the user guesses a location where there is a battleship, display battleship image
+	}
+
+
+};
+
