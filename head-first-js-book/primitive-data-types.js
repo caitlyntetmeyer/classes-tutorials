847a3e7fc986877df4208d9ae31506ee28df9b81
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-28 16:15:02 -0400

    Primitive data types

---
+++ head-first-js-book/primitive-data-types.js
@@ -0,0 +1,18 @@
+// 5 Primitive Datatypes in JavaScript
+
+// Numbers (whole, fractional, negative)
+4
+9.3
+-10
+
+// Strings (text inside of quotes)
+"Hello World"
+"43"
+
+// Booleans
+true
+false
+
+// null and undefined
+null
+undefined
