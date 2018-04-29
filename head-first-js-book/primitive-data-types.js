0faa0383f41b03b7fba104f3ce175304c6ef957e
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-04-29 19:04:30 -0400

    Notes on strings in JS

--- head-first-js-book/primitive-data-types.js
+++ head-first-js-book/primitive-data-types.js
@@ -8,6 +8,24 @@
 // Strings (text inside of quotes)
 "Hello World"
 "43"
+	// Single or double quotes are okay
+	"hello world"
+	'hello world'
+
+	// Concatenation
+	"charlie" + "brown" // "charliebrown"
+	"hi " + "goodbye" // "hi goodbye"
+
+	// Escape characters start with "\"
+	"Singin \"Do wah diddy\""
+	"This is a backslash: \\"
+
+	// Strings have a length property
+	"hello world".length // 11
+
+	// Access individual characters using [] and an index
+	"hello"[0] // "h"
+	"hello"[4] // "o"
 
 // Booleans
 true
