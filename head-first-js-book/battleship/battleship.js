81739bb6397b3dae836e70324728989f7cd3e27d
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-05-01 18:53:38 -0400

    Worked on the view for the battleship game

--- head-first-js-book/battleship/battleship.js
+++ head-first-js-book/battleship/battleship.js
@@ -6,11 +6,24 @@ var view = {
 		// Update the text of the messageArea element by setting its innerHTML to msg:
 		messageArea.innerHTML = msg;
 	},
+		displayHit: function(location) {
+		// If the user guesses a location where there is a battleship, display battleship image
+		// Get the element by its id, which matches the location:
+		var cell = document.getElementById(location);
+		// Set the class attribute of the element to hit:
+		cell.setAttribute("class", "hit");
+	},
 	displayMiss: function(location) {
 		// If the user guesses a location where there's no battleship, display MISS image
-	},
-	displayHit: function(location) {
-		// If the user guesses a location where there is a battleship, display battleship image
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
 	}
 };
 
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap, tap, is this thing on?");
