012e7b99ccc2bcf6b477ec71b92fbc5de8489343
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-05-01 18:33:03 -0400

    Notes on strings

--- udemy-web-developer-bootcamp/javascript/variables.js
+++ udemy-web-developer-bootcamp/javascript/variables.js
@@ -1,1 +1,16 @@
 // Variables are containers that store values.
+
+var name = "Rusty"; // name is Rusty
+name = "Tater"; // name is now Tater
+
+var friend = "Sally";
+"Hello, " + friend; // Hello, Sally
+friend = "no one" 
+"Hello, " + friend; // Hello, no one
+
+var secretNumber = 73;
+
+var isAdorable = true;
+
+var num = 37;
+num + 1 // 38
