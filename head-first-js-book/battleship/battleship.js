8f64d2d8e07a0c4ec21f55ba180240f6d3578807
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-05-12 22:20:56 -0400

    Implemented core game logic and display

--- head-first-js-book/battleship/battleship.js
+++ head-first-js-book/battleship/battleship.js
@@ -96,19 +96,19 @@ var model = {
 
 
 // Test:
-model.fire("53");
+// model.fire("53");
 
-model.fire("06");
-model.fire("16");
-model.fire("26");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
 
-model.fire("34");
-model.fire("24");
-model.fire("44");
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
 
-model.fire("12");
-model.fire("11");
-model.fire("10");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
 
 
 // The controller will get and process the player's guess, keep track of the number of guesses, ask the model to update itself based on the latest guess, and detect when the game is over (when all ships have been sunk).
@@ -120,46 +120,58 @@ var controller = {
 
 	// guess will have a form like "A0":
 	processGuess: function(guess) {
-
-		function parseGuess(guess) {
-			var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
-
-			if (guess === null || guess.length !== 2) {
-				alert("Oops, please enter a letter and a number on the board.");
-			} else {
-				// Grab the first character of the guess:
-				var firstChar = guess.charAt(0);
-				// Use indexOf to get back a number between 0 and 6 that corresponds to the letter:
-				var row = alphabet.indexOf(firstChar);
-
-				var column = guess.charAt(1);
-
-				if (isNaN(row) || isNaN(column)) {
-					alert("Oops, that isn't on the board.");
-				} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
-					alert("Oops, that's off the board!");
-				} else {
-					return row + column;
-				}
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
 			}
-			return null;
-
 		}
-console.log(parseGuess("A0"));
-console.log(parseGuess("B6"));
-console.log(parseGuess("G3"));
-console.log(parseGuess("H0"));
-console.log(parseGuess("A7"));
 	}
-
-
 };
 
+function parseGuess(guess) {
+	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
+	if (guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board.");
+	} else {
+		// Grab the first character of the guess:
+		var firstChar = guess.charAt(0);
+		// Use indexOf to get back a number between 0 and 6 that corresponds to the letter:
+		var row = alphabet.indexOf(firstChar);
 
+		var column = guess.charAt(1);
 
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board.");
+		} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+			alert("Oops, that's off the board!");
+		} else {
+			return row + column;
+		}
+	}
+	return null;
 
+}
 
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
+
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
 
 
 
