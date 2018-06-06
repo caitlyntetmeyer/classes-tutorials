d259ffb423453863d4849a400f229bc5b8cfb730
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-06-06 19:31:11 -0400

    User Experience notes

---
+++ ux.js
@@ -0,0 +1,21 @@
+/*
+
+UX - User Experience (Career Foundry UX Design Short Course)
+
+	-the overall experience a user has when interacting with a product or service
+	-term UX coined in 1990s by Dr. Donald Norman at Apple
+	importance of user-centered design - design process based on needs and goals of user
+
+UX Design (UXD)
+	
+	-designing specifically for the needs of the user or customer
+	-ease of use, quality, efficiency
+	-intuitive and straightforward as possible
+
+Wireframes
+	-drawing of a design
+	-used to communicate design ideas, demonstrate how a design will function
+
+Good UX is useful, usable, desirable, findable, accessible, credible
+
+*/
