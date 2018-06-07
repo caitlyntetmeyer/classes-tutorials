042b499b5282f7066e2daa095ae72bc883bab779
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-06-07 18:57:34 -0400

    UX course day 2 notes

--- ux.js
+++ ux.js
@@ -2,6 +2,11 @@
 
 UX - User Experience (Career Foundry UX Design Short Course)
 
+
+Lesson 1
+
+UX
+
 	-the overall experience a user has when interacting with a product or service
 	-term UX coined in 1990s by Dr. Donald Norman at Apple
 	importance of user-centered design - design process based on needs and goals of user
@@ -17,5 +22,20 @@ Wireframes
 	-used to communicate design ideas, demonstrate how a design will function
 
 Good UX is useful, usable, desirable, findable, accessible, credible
+
+
+Lesson 2 - A Typical UX Design Process
+
+	User Research
+
+	User Persona
+
+	User Stories
+
+	Wireframes and Prototypes
+
+	Testing
+
+	Visual Design and Development
 
 */
