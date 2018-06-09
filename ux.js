642e9a3e7b84ff5a978d27f962a70045211d934f
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-06-09 19:18:37 -0400

    Day 4 of UX course

--- ux.js
+++ ux.js
@@ -48,9 +48,20 @@ Lesson 4 - Skills
 	-Organization
 
 	Hard Skills:
-	
+
 	-User Research
 	-Information Architecture
 	-Wireframing and Prototyping
+
+
+Lesson 5 - UX Design Jobs and the Quadrant Model
+
+	-Experience Strategy (ExS) -  incorporating business, technical, and design strategies into viable solutions that bring value to both the user and the company providing the product - product designer, product manager
+
+	-User Research (UR) - focus on interactions with real users, collecting and analyzing both qualitative and quantitative data to better inform design decisions - user experience researcher, usability analyst, research analyst
+
+	-Interaction Design (IxD) - integrates experience strategy, user research, and information architecture into an intuitive interface that brings value to users by reducing the friction between user and product - interaction designer, product designer, and experience designer
+
+	-Information Architecture (IA) - organization of product content and information to optimize the user’s experience - information architect, data analyst, content strategist
 
 */
