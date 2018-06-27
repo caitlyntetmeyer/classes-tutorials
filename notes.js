5352ad50e58c0c757efccb7c1e9dee507259d989
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-06-27 15:35:06 -0400

    Started MongoDB tutorial on The Net Ninja YouTube channel

---
+++ notes.js
@@ -0,0 +1,26 @@
+/*
+
+MongoDB course - The Net Ninja
+
+https://www.youtube.com/watch?v=9OPP_1eAENg&list=PL4cUxeGkcC9jpvoYriLI0bY8DOgWZfi6u
+
+MongoDB
+-NoSQL Database (data's stored in documents or collections of objects)
+-Use it any time you want to store data and persist it in your app
+-Mongo is the "M" in MEAN
+
+Client (browser) -> Server (Node); Mongoose -> MongoDB
+
+Mongoose - package we can install on Node.js (on the server) which makes communicating with MongoDB easier
+
+In this course...
+-Install MongoDB and Mongoose
+-Use basic CRUD operations - Create, Read, Update and Delete
+-Mocha (testing framework)
+
+What you need to know...
+-JavaScript
+-Install Node.js (for npm)
+
+
+*/
