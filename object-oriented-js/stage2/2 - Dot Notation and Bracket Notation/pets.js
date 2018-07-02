c3beab394504c1e5bd4fb1698da6c8e36b1c359b
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-02 15:15:27 -0400

    Completed a section of OOP course

--- object-oriented-js/stage2/2 - Dot Notation and Bracket Notation/pets.js
+++ object-oriented-js/stage2/2 - Dot Notation and Bracket Notation/pets.js
@@ -1,4 +1,4 @@
-const ernie = {
+/*const ernie = {
 	// There are 3 properties here:
     animal: 'dog', // This is a key-value pair.
     age: '1',
@@ -7,7 +7,28 @@ const ernie = {
     bark: function(){
         console.log('Woof!');
     } // Call this using bark()
+}*/
+
+const ernie = {
+    animal: 'dog',
+    age: '1',
+    breed: 'pug',
+    bark: function(){
+        console.log('Woof!');
+    } 
 }
+
+// ernie.age = '2';
+ernie['age'] = '2';
+
+ernie.color = 'black';
+console.log(ernie); /* { animal: 'dog',
+  age: '2',
+  breed: 'pug',
+  bark: [Function: bark],
+  color: 'black' } */
+
+
 
 // Important vocab: encapsulation - one of the main principles with OOP - put all the inner workings of an object inside that object. 
 
@@ -20,9 +41,11 @@ console.log(ernie.breed); // pug
 ernie.bark(); // Woof!
 */
 
+/*
 console.log(ernie['age']); // 1
 console.log(ernie['breed']); // pug
 ernie['bark'](); // Woof!
 
 var prop = 'breed';
 console.log(ernie[prop]); // pug
+*/
