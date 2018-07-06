f6c59ca0ce35424231eb4517ad8dc40cd08aa8c4
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-06 18:26:11 -0400

    Method arguments in Ruby

---
+++ Ruby Basics/parameters.rb
@@ -0,0 +1,22 @@
+def add(first, second)
+	puts first, second
+	puts first + second
+end
+
+def subtract(first, second)
+	puts first, second
+	puts first - second
+end
+
+add 100, 50
+# 100
+# 50
+# 150
+
+subtract 75, 25
+# 75
+# 25
+# 50
+
+add 3, 4
+subtract 10, 5
