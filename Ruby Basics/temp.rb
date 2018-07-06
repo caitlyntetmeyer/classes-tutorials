d756f90d361a96ef8ac7354ee7885b09dcbbe0cd
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-06 18:04:57 -0400

    Methods in Ruby

--- Ruby Basics/temp.rb
+++ Ruby Basics/temp.rb
@@ -1,2 +1,23 @@
-# Prints "hello world" to the terminal
-puts "hello world" # hello world
+def wait
+  puts "Waiting..."
+  sleep 3
+  puts "Done"
+end
+
+def count_to_three  
+  puts 1
+  puts 2
+  puts 3
+end
+
+count_to_three
+wait
+count_to_three
+count_to_three
+
+# From the quiz:
+def say_hi
+  puts "hi"
+end
+
+say_hi
