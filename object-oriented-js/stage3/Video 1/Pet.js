5623cf16e6698850af65dc6801c5cf86eecf3eee
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-05 20:43:01 -0400

    Added a method to the Pet class

--- object-oriented-js/stage3/Video 1/Pet.js
+++ object-oriented-js/stage3/Video 1/Pet.js
@@ -1,17 +1,26 @@
 class Pet {
-	constructor(animal, age, breed) {
+	constructor(animal, age, breed, sound) {
 		this.animal = animal;
 		this.age = age;
 		this.breed = breed;
+		this.sound = sound;
 	}
+
+	speak() {
+		console.log(this.sound);
+	}
+
 }
 
-const ernie = new Pet('dog', 1, 'pug');
-const vera = new Pet('dog', 8, "Border Collie");
-const scofield = new Pet('dog', 6, "Doberman");
-const edel = new Pet('dog', 7, "German Shorthaired Pointer");
+const ernie = new Pet('dog', 1, 'pug', 'yip yip');
+const vera = new Pet('dog', 8, "Border Collie", 'woof woof');
+const scofield = new Pet('dog', 6, "Doberman", 'Woof Woof');
+const edel = new Pet('dog', 7, "German Shorthaired Pointer", 'woof');
 
-console.log(ernie); // Pet { animal: 'dog', age: 1, breed: 'pug' }
+// console.log(ernie); // Pet { animal: 'dog', age: 1, breed: 'pug' }
+
+ernie.speak(); // yip yip
+vera.speak(); // woof woof
 
 /*
 Exercise:
