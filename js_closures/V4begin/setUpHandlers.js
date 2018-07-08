1d7ea57825fdb6e0e2f6a4d767d906c3eddf6b60
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-08 12:57:38 -0400

    Completed Closures workshop on Treehouse

--- js_closures/V4begin/setUpHandlers.js
+++ js_closures/V4begin/setUpHandlers.js
@@ -1,9 +1,29 @@
 var buttons = document.getElementsByTagName('button');
 
+function createHandler(name) {
+	return function(){
+		console.log(name);
+	}
+}
+
 for(var i = 0; i < buttons.length; i += 1) {
 	var button = buttons[i];
 	var buttonName = button.innerHTML;
-	button.addEventListener('click', function() {
-		console.log(buttonName);
-	});
+	button.addEventListener('click', createHandler(buttonName));
 }
+
+/*
+
+Express example:
+
+app.use(function(req, res, next) {
+	// middleware logic
+	next();
+});
+
+Morgan example:
+
+var logger = require('morgan');
+app.use(logger('dev'));
+
+*/
