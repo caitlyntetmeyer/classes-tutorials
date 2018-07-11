05ae5006b5208e2a8845d1aeaef939939cf60bdf
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-10 21:35:32 -0400

    Continuin AngularJS course on Treehouse - services

--- treehouse-angularjs-basics/scripts/app.js
+++ treehouse-angularjs-basics/scripts/app.js
@@ -1,6 +1,1 @@
-angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope){
-	$scope.helloWorld = function() {
-		console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
-	};
-});
+angular.module("todoListApp", []);
