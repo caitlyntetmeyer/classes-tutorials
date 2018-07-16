99b5778d9fafcc0a7972e94b6787229451a60061
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-16 18:59:27 -0400

    Continued the DOM with getElementsByClassName

--- JavaScript/the-DOM/vid-1-simple-example/start/app.js
+++ JavaScript/the-DOM/vid-1-simple-example/start/app.js
@@ -5,6 +5,12 @@ for (let i = 0; i < myList.length; i++) {
 	myList[i].style.color = 'purple';
 }
 
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i++) {
+	errorNotPurple[i].style.color = 'red';
+}
+
 /*
 
 In Console:
