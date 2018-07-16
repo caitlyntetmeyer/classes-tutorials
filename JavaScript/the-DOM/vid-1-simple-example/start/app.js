7c82155e17e32844e1daddce944a50317ca0d61f
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-16 13:13:04 -0400

    Used getElementsByTagName to turn li's purple

--- JavaScript/the-DOM/vid-1-simple-example/start/app.js
+++ JavaScript/the-DOM/vid-1-simple-example/start/app.js
@@ -1,4 +1,24 @@
-const myHeading = document.getElementById('myHeading');
+const myList = document.getElementsByTagName('li');
+
+// Turn the li's purple:
+for (let i = 0; i < myList.length; i++) {
+	myList[i].style.color = 'purple';
+}
+
+/*
+
+In Console:
+
+const myList = document.getElementsByTagName('li');
+	undefined
+myList[2].style.color = 'purple';
+	"purple"
+
+*/
+
+/*
+
+const myHeading = document.getElementsByTagName('h1')[0];
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
@@ -6,3 +26,23 @@ const myTextInput = document.getElementById('myTextInput');
 myButton.addEventListener('click', () => {
 	myHeading.style.color = myTextInput.value;
 });
+
+*/
+
+/*
+
+In the console, turn the paragraph blue:
+
+const myParagraph = document.getElementsByTagName('p');
+	undefined
+
+myParagraph.length;
+	1
+
+myParagraph[0];
+	<p>​Making a web page interactive​</p>​
+
+myParagraph[0].style.color = 'blue';
+	"blue"
+
+*/
