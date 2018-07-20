03280db4711b31e400e3005cdf777dd21ea21600
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-20 16:36:54 -0400

    Completed DOM course

--- JavaScript/the-DOM/5-traversing-the-DOM/vid-1-using-parent-node/start/app.js
+++ JavaScript/the-DOM/5-traversing-the-DOM/vid-1-using-parent-node/start/app.js
@@ -6,6 +6,33 @@ const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const lis = listUl.children;
+const firstListItem = listUl.firstElementChild;
+const lastListItem = listUl.lastElementChild;
+
+firstListItem.style.backgroundColor = 'lightskyblue';
+lastListItem.style.backgroundColor = 'lightsteelblue';
+
+function attachListItemButtons (li) {
+  let up = document.createElement('button');
+  up.className = "up";
+  up.textContent = "Up";
+  li.appendChild(up);
+
+  let down = document.createElement('button');
+  down.className = "down";
+  down.textContent = "Down";
+  li.appendChild(down);
+
+  let remove = document.createElement('button');
+  remove.className = "remove";
+  remove.textContent = "Remove";
+  li.appendChild(remove);
+}
+
+for (let i = 0; i < lis.length; i++) {
+  attachListItemButtons(lis[i]);
+}
 
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
@@ -20,6 +47,14 @@ listUl.addEventListener('click', (event) => {
       let ul = li.parentNode;
       if (prevLi) {
       ul.insertBefore(li, prevLi);
+      }
+    }
+    if (event.target.className == "down") {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      let ul = li.parentNode;
+    if (nextLi) {
+      ul.insertBefore(nextLi, li);
       }
     }
   }
@@ -44,7 +79,15 @@ addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  attachListItemButtons(li);
   ul.appendChild(li);
   addItemInput.value = '';
 });
+
+
+
+
+
+
+
   
