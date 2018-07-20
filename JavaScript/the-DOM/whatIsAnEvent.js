56e14f5975e5b0f7f222235ed085bba4d2ce5e36
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-20 15:38:33 -0400

    Completed "Responding to User Interaction"

---
+++ JavaScript/the-DOM/whatIsAnEvent.js
@@ -0,0 +1,29 @@
+// an event - sth you do on a webpage
+// Browsers listen for these events and can do sth in response
+/*
+
+Events:
+
+click
+dblclick
+mousedown
+mouseup
+mousemove
+mouseover
+mouseout
+
+touchstart
+touchmove
+touchend
+
+keydown
+keyup
+keypress
+
+load
+
+---
+
+Event Reference: https://developer.mozilla.org/en-US/docs/Web/Events
+
+*/
