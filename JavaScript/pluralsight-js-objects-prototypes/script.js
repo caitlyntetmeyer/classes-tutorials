104eaacf4dcd1bac55d855429d514bee57119233
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-28 16:41:40 -0400

    Using Object.create()

--- JavaScript/pluralsight-js-objects-prototypes/script.js
+++ JavaScript/pluralsight-js-objects-prototypes/script.js
@@ -1,15 +1,28 @@
 'use strict';
 
-function Cat(name, color) {
-	this.name = name
-	this.color = color
-}
-// new creates a new empty JS object
-var cat = new Cat('Fluffy', 'White');
+// function Cat(name, color) {
+// 	this.name = name
+// 	this.color = color
+// }
+// // new creates a new empty JS object
+// var cat = new Cat('Fluffy', 'White');
+
+var cat = Object.create(Object.prototype,
+{
+	name: {
+		value: 'Fluffy',
+		enumerable:true,
+		writable:true,
+		configurable:true
+	},
+	color: {
+		value:'White',
+		enumerable:true,
+		writable:true,
+		configurable: true
+	}
+})
 
 display(cat)
-/* Cat {
-	name: Fluffy
-	color: White
-} */
+
 
