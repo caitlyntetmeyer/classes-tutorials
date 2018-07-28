1ad67e290c847dd0544c00865da56e69c8619755
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-28 16:43:38 -0400

    Using ES6 Classes

--- JavaScript/pluralsight-js-objects-prototypes/script.js
+++ JavaScript/pluralsight-js-objects-prototypes/script.js
@@ -1,28 +1,17 @@
 'use strict';
 
-// function Cat(name, color) {
-// 	this.name = name
-// 	this.color = color
-// }
-// // new creates a new empty JS object
-// var cat = new Cat('Fluffy', 'White');
-
-var cat = Object.create(Object.prototype,
-{
-	name: {
-		value: 'Fluffy',
-		enumerable:true,
-		writable:true,
-		configurable:true
-	},
-	color: {
-		value:'White',
-		enumerable:true,
-		writable:true,
-		configurable: true
+class Cat {
+	constructor(name, color) {
+		this.name = name
+		this.color = color
 	}
-})
+
+	speak() {
+		display('Meeooow')
+	}
+}
+
+var cat = new Cat('Fluffy', 'White')
 
 display(cat)
-
-
+cat.speak() // Meeooow
