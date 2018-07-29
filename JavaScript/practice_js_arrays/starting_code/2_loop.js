a447725a8df927e2a6fc6a4a1fc2764019401545
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-29 13:26:11 -0400

    Array Iteration with a For Loop

--- JavaScript/practice_js_arrays/starting_code/2_loop.js
+++ JavaScript/practice_js_arrays/starting_code/2_loop.js
@@ -14,21 +14,21 @@ function random100() {
 
 /* 1. Create a function named createRandomList that uses a for loop to create an array containing 10 random numbers from 1 to 100 (use the supplied function above to generate the numbers). The function should return that array. */
 function createRandomList() {
-	var ranNums = [];
-	for (i = 0; i < 10; i++) {
+	let ranNums = [];
+	for (let i = 0; i < 10; i++) {
 		ranNums.push(random100());
 	}
 	return ranNums;
 }
 
 /* 2. Call the createRandomList() function and store the results in a variable named myRandomList. */
-var myRandomList = createRandomList();
+let myRandomList = createRandomList();
 
 /* 3. Use a for loop to access each element in the loop. Each time through the loop log a message to the console that looks something like this:
 Item 0 in the array is 48
 When you're done you should have output 10 lines to the console -- one for each element. 
 */
-for (i = 0; i < myRandomList.length; i++) {
+for (let i = 0; i < myRandomList.length; i++) {
 	console.log("Item " + i + " in the array is " + myRandomList[i]);
 }
 
