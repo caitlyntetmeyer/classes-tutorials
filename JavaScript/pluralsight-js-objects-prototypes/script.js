89774c6fd608749cecf52167d80e03a8e4e64c28
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-29 12:54:47 -0400

    What is a Prototype?

--- JavaScript/pluralsight-js-objects-prototypes/script.js
+++ JavaScript/pluralsight-js-objects-prototypes/script.js
@@ -1,10 +1,21 @@
 'use strict';
 
-var arr = new Array('red', 'blue', 'green')
+function Cat(name, color) {
+	this.name = name
+	this.color = color
+}
+var fluffy = new Cat('Fluffy', 'White')
 
-Object.defineProperty(Array.prototype, 'last', {get: function() {
-	return this[this.length-1]
-}})
-var last = arr.last
-var arr2 = ['one', 'two', 'three']
-display(Array) // function Array() { [native code] }
+Cat.prototype.age = 3
+
+display(Cat.prototype) // Cat {age: 3}
+display(fluffy.__proto__) // Cat {age: 3}
+
+var muffin = new Cat('Muffin', 'Brown')
+
+display(muffin.__proto__) // Cat {age: 3}
+
+
+// function's prototype - the object instance that'll become the prototype for all objects created using this function as a constructor
+
+// object's prototype - the object instance from which the object is inherited
