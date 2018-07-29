58c6c648cf6dc575856b851e064dee90bd229334
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-29 12:34:48 -0400

    Using Getters and Setters

--- JavaScript/pluralsight-js-objects-prototypes/script.js
+++ JavaScript/pluralsight-js-objects-prototypes/script.js
@@ -4,7 +4,20 @@ var cat = {
 	name: {first:'Fluffy', last: 'LaBeouf'},
 	color: 'White',
 }
+// Get full name of cat:
+Object.defineProperty(cat, 'fullName',
+{
+	get: function() {
+		return this.name.first + " " + this.name.last
+	},
+	set: function(value) {
+		var nameParts = value.split(" ")
+		this.name.first = nameParts[0]
+		this.name.last = nameParts[1]
+	}
+})
 
-delete cat.name 
-
-display(cat.name) // undefined
+cat.fullName = 'Muffin Top'
+display(cat.fullName) // Muffin Top
+display(cat.name.first) // Muffin
+display(cat.name.last) // Top
