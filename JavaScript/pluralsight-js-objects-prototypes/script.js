932297cfd792c05505a753e9b1a71c17c79b06e8
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-29 12:30:02 -0400

    Using the Configurable Attribute

--- JavaScript/pluralsight-js-objects-prototypes/script.js
+++ JavaScript/pluralsight-js-objects-prototypes/script.js
@@ -1,9 +1,10 @@
 'use strict';
-// enumerable means we can loop over them using a for in loop
+
 var cat = {
 	name: {first:'Fluffy', last: 'LaBeouf'},
 	color: 'White',
 }
 
-Object.defineProperty(cat, 'name', {enumerable: false})
+delete cat.name 
 
+display(cat.name) // undefined
