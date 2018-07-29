68ea448ebb6074dbcbcda4b5075752c8802e2471
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-07-29 13:02:02 -0400

    Instance vs. Prototype Properties

--- JavaScript/pluralsight-js-objects-prototypes/script.js
+++ JavaScript/pluralsight-js-objects-prototypes/script.js
@@ -4,16 +4,17 @@ function Cat(name, color) {
 	this.name = name
 	this.color = color
 }
+Cat.prototype.age = 4
+
 var fluffy = new Cat('Fluffy', 'White')
-
-Cat.prototype.age = 3
-
-display(Cat.prototype) // Cat {age: 3}
-display(fluffy.__proto__) // Cat {age: 3}
-
 var muffin = new Cat('Muffin', 'Brown')
 
-display(muffin.__proto__) // Cat {age: 3}
+fluffy.age = 5
+
+display(fluffy.age) // 5
+display(fluffy.__proto__.age) // 4
+
+display(fluffy.hasOwnProperty('age')) // true
 
 
 // function's prototype - the object instance that'll become the prototype for all objects created using this function as a constructor
