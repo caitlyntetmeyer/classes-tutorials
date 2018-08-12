b3f14ab8948c176bc9979165b794293ba00a70e8
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-08-12 19:22:23 -0400

    What Is Validation

---
+++ JavaScript/regular-expressions-in-js/08-what-is-validation.js
@@ -0,0 +1,13 @@
+// To write a regular expression in JavaScript, you can put the expression between two slashes:
+
+const regexObject = /^word$/
+// or:
+const regexObject = new RegExp("^word$")
+
+// ---
+
+// Literal:
+/regexpString/
+
+// Constructor:
+new RegExp("regexpString")
