df2dfb9c44a03150348b068a708cf43b7ceb5b46
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-08-18 15:04:14 -0400

    A Better Telephone Number Validator

---
+++ JavaScript/regular-expressions-in-js/12-phone-num-validator.js
@@ -0,0 +1,5 @@
+// A Better Telephone Number Validator
+
+// Let's say customers are required to type their phone number in this format: (123) 456-7890
+// That doesn't give them much flexibility. Let's re-format the number for them instead.
+// See 03_reformatting_user_input
