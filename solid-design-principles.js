767bfe2e27fba5e1753ffeb80ea5ffdd2edce591
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-08-19 19:06:29 -0400

    SOLID design principles; started CSS Selectors course

---
+++ solid-design-principles.js
@@ -0,0 +1,29 @@
+/*
+
+SOLID Design Principles:
+
+SOLID is a set of design principles in object-oriented software development.
+
+S - Single Responsibility Principle
+O - Open/Closed Principle
+L - Liskov Substitution Principle
+I - Interface Segregation Principle
+D - Dependency Inversion
+
+---
+
+Single Responsibility Principle:
+
+A class should have one, and only one, reason to change.
+
+Why follow this? It makes your software easier to implement and prevents unexpected side effects of future changes.
+
+Classes, microservices, and software components that have only one responsibility are much easier to explain and understand. This reduces the number of bugs, improves your development speed, and makes your life as a software dev much easier.
+
+
+
+
+
+https://stackify.com/solid-design-principles/?utm_referrer=https%3A%2F%2Fwww.google.com%2F
+
+*/
