ac7fb0fd633bd0d1001192e5ee0203ec66de87ad
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-09-06 20:25:29 -0400

    Blog Previewer: Changing Content Inside Elements

--- jQuery/jquerybasics_coursefiles/1_blog_previewer/video5/js/app.js
+++ jQuery/jquerybasics_coursefiles/1_blog_previewer/video5/js/app.js
@@ -3,3 +3,21 @@ $('#flashMessage')
   .slideDown(1000)
   .delay(3000)
   .slideUp();
+
+/* 
+$('#element').html(); // getter
+$('#element').html("<p>I am setting this HTML!</p>"); // setter 
+
+Get:
+
+$('.blogPage-title').text() // "New Blog Post"
+$('.blogNewPost').html(); // (all the HTML in the element)
+
+*/
+
+const title = "My First Blog Post";
+const content = "This is my <strong>first</strong> post!";
+
+$("#blogTitlePreview").text(title);
+
+$('#blogContentPreview').html(content); // must use .html() to render HTML
