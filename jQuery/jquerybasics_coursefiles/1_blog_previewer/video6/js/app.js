d0da2f9423084216df7bfd27009877fe6ba7a149
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-09-08 16:26:36 -0400

    Blog Previewer: Getting Values from Form Fields

--- jQuery/jquerybasics_coursefiles/1_blog_previewer/video6/js/app.js
+++ jQuery/jquerybasics_coursefiles/1_blog_previewer/video6/js/app.js
@@ -1,11 +1,19 @@
-$('#flashMessage')
-  .hide()
-  .slideDown(1000)
-  .delay(3000)
-  .slideUp();
 
-const title = "My First Blog Post";
-const content = "This is my <strong>first</strong> post!";
 
-$("#blogTitlePreview").text(title);
-$("#blogContentPreview").html(content);
+// const title = "My First Blog Post";
+// const content = "This is my <strong>first</strong> post!";
+
+$('#flashMessage').hide();
+
+$("#previewButton").click(function() {
+	const title = $("#blogTitleInput").val(); // retrieve what the user entered
+	const content = $("#blogContentInput").val();
+
+	$("#blogTitlePreview").text(title);
+	$("#blogContentPreview").html(content);
+
+	$('#flashMessage')
+	  .slideDown(1000)
+	  .delay(3000)
+	  .slideUp();
+});
