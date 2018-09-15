9587085bc263d7ab26acebf23b372f72f471c231
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-09-14 20:57:46 -0400

    Using on() for Event Handling

--- jQuery/jquerybasics_coursefiles/2_spoiler/video5/js/app.js
+++ jQuery/jquerybasics_coursefiles/2_spoiler/video5/js/app.js
@@ -6,14 +6,9 @@ $('.spoiler').append($button);
 //Hide the spoiler text
 $('.spoiler span').hide();
 // When the button is pressed 
-$('.spoiler button').click(function(){
+$('.spoiler button').on('click', function(){
   //Show the spoiler text
   $('.spoiler span').show();
   //Hide the "Reveal Spoiler" button
   $('.spoiler button').hide();
 });
-
-
-
-
-
