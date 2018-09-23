6a007f532da3f1da257b87b0361ae5fa3cff30a3
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-09-22 20:10:19 -0400

    Changing Element Styles and Classes

--- jQuery/jquerybasics_coursefiles/3_newsfeed/video1/js/app.js
+++ jQuery/jquerybasics_coursefiles/3_newsfeed/video1/js/app.js
@@ -2,4 +2,6 @@ const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
 
-$pdfs.hide();
+$secureLinks.attr('target', '_blank');
+$pdfs.attr('download', true);
+
