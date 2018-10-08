34edfaaa9ffcea30a9906b6a536d16c865683b01
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-10-08 18:48:02 -0400

    How to learn like a pro; focus your mind

---
+++ how-to-learn-treehouse.js
@@ -0,0 +1,51 @@
+/*
+
+Treehouse: How to Learn
+
+Learn Like an Expert - 
+
+	Identify areas of weakness and work to improve on those
+
+	Identifying My Learning Weaknesses:
+	-What are the main concepts/ideas/facts I just learned?
+	-Can I explain them in my own words?
+	-Can I relate them to things I already know?
+
+	If you can't answer those questions, you've ID'ed a learning weakness. Don't continue to the next chapter, video or lesson.
+
+	If you're not understanding:
+	-Put it into action
+	-Rewatch or re-read with purpose
+	-Supplement what I learned with another source
+
+	If you're still confused:
+	-Seek advice from an expert or colleague
+	-Go to Community in Treehouse and ask
+	-See if there's a Slack or IRC channel
+	-Don't give up!
+
+Focus Your Mind, Improve Your Learning -
+
+	You need both focus and diffuse modes
+
+	Prepare your body
+		-Exercise clears toxins from our brain
+		-Fuel your brain with healthy fats like eggs, nuts and avocado
+		-Wait to have coffee til mid-morning
+
+	Prepare your mind
+		-Break your time into chunks of focus (1 hr) and diffuse (15 min.)
+
+	Prepare your space
+		-Close applications you don't need and turn off notifications
+		-Wear hdeadphones - listen to nature sounds or white noise
+
+	Learn sth new before bed
+	Get started! Work on sth you've been putting off for just 5 minutes
+
+3 Ways to Build a Habit of Learning - 
+	
+	
+
+
+*/
