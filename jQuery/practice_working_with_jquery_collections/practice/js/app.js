58a3963704dd420bb596b9b535c78235a55eb8fb
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-10-11 19:15:47 -0400

    Added a class using jQuery

--- jQuery/practice_working_with_jquery_collections/practice/js/app.js
+++ jQuery/practice_working_with_jquery_collections/practice/js/app.js
@@ -5,9 +5,10 @@ $secondItem = $('li a[href*="portofcallnola"]');
   //  Add a class of "featured" to the new span element. 
   $featuredSpan.addClass('featured');
 //2. Add a class of "new" to the last item in the list. You can traverse to the last item or find a jQuery method that helps you select the last item in the list. 
-
+$("li").last().addClass("new");
 
 //3. Add an attribute to each link so that all anchor tags open in a new tab. 
+
 
 //4. Select and print the index and text of each anchor tag to the console
 
