44a48f41246046cf81b6b58e96183eb6fcf22c6d
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-10-12 18:06:23 -0400

    Attempted jQuery exercise on my own

--- jQuery/practice_working_with_jquery_collections/practice/js/app.js
+++ jQuery/practice_working_with_jquery_collections/practice/js/app.js
@@ -1,5 +1,7 @@
 //1. Add a "featured" tag to the second item in the list: 
 $secondItem = $('li a[href*="portofcallnola"]');
+$secondItem.addClass("featured");
+
   //  Append a new span element containing the word "Featured" to the list item
   $featuredSpan = $secondItem.append(' <span>Featured</span>');
   //  Add a class of "featured" to the new span element. 
@@ -8,10 +10,12 @@ $secondItem = $('li a[href*="portofcallnola"]');
 $("li").last().addClass("new");
 
 //3. Add an attribute to each link so that all anchor tags open in a new tab. 
-
+$("a").attr("target", "_blank");
 
 //4. Select and print the index and text of each anchor tag to the console
-
+$("a").each(function( index ) {
+	console.log( index + ": " + $(this).text() )
+});
 
 //5. Refactor question #4 using the 'this' keyword. 
 
