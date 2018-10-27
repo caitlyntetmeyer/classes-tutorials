d512812709d79c9cadb8a298ef3d0762887b1b20
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-10-27 18:11:11 -0400

    Started Game Class Constructor Method

--- JavaScript/ObjectOrientedJavaScriptByExample/Stage1/Stage1Step1/js/Game.js
+++ JavaScript/ObjectOrientedJavaScriptByExample/Stage1/Stage1Step1/js/Game.js
@@ -1,3 +1,19 @@
 class Game {
-	
+	constructor(board, players, ready) {
+		this.board = new Board();
+		this.players = createPlayers();
+		this.ready = false;
+	}
+
+	/** 
+	 * Creates two player objects
+	 * @return  {Array}    An array of two Player objects.
+	 */
+
+	createPlayers(name, color, id, active) {
+		name: "Player 1",
+		color: "e15258",
+		id: 1,
+		active: true
+	}
 }
